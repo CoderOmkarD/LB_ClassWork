d495e9651e15e9d4732ec54186e6e4779c1eb166
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-17 23:26:31 +0530

    Program 71 to 80

---
+++ program80.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Number {
+    public boolean CheckPerfect(int iNo) {
+        int icnt = 0;
+        int iSum = 0;
+
+        if (iNo < 0) {
+            iNo = -iNo;
+        }
+        for (icnt = 1; icnt <= iNo / 2; icnt++) {
+            if ((iNo % icnt) == 0) {
+                iSum = iSum + icnt;
+            }
+        } // Time Complexity O(N/2)
+
+        if (iSum == iNo) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+// End of Number Class
+
+class program80 {
+    public static void main(String A[]) {
+        int iValue = 0;
+        boolean bRet = false;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+        bRet = nobj.CheckPerfect(iValue);
+
+        if (bRet == true) {
+            System.out.print(iValue + " is a Perfect Number");
+        } else {
+            System.out.print(iValue + " is not a Perfect Number");
+        }
+
+        // Important
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+
+    }
+}
