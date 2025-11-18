b4815b2be683e8eb6f3521061069f3658d3d9e1d
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-18 22:24:26 +0530

    Program 81 to 90

---
+++ program87.java
@@ -0,0 +1,49 @@
+//Input =5
+//Output = 5*4*3*3*2*1
+
+import java.util.*;
+
+class Number {
+    public int CalculateFactorial(int iNo) {
+        int iCnt = 0;
+        int iFact = 1;
+
+        /// Updator///
+        if (iNo < 0) {
+            iNo = -iNo;
+        }
+
+        // Reverse Loop
+        for (iCnt = 1; iCnt <= iNo; iCnt++) {
+            iFact = iFact * iCnt;
+
+        }
+        return iFact; // Change
+    }
+}
+// End of Number Class
+
+class program87 {
+    public static void main(String A[]) {
+        int iValue = 0;
+        int iRet = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+
+        iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+        iRet = nobj.CalculateFactorial(iValue);
+
+        System.out.print("Factorial of " + iValue + " is : " + iRet);
+
+        // Important
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+
+    }
+}
