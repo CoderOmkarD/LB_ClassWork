4a9be2faba20a2610ad086f91522a1283a46a545
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-23 23:57:17 +0530

    Problems On N numbers

---
+++ program120.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+void Display(int Arr[])
+{
+    int iCnt = 0;
+
+    for (iCnt = 0; iCnt < 7; iCnt++)
+    {
+        printf("%d\n", Arr[iCnt] );//
+        //The * (Arr + 0 ) is same as Arr[0]
+        //The * (Arr + 1 ) is same as Arr[1]
+        //The * (Arr + 2 ) is same as Arr[2]
+        //The * (Arr + 3 ) is same as Arr[3]
+        // Thats why * (Arr + iCnt) is same as Arr[iCnt]
+
+    }
+}
+int main()
+{
+    int Arr[] = {10, 20, 30, 40,50,60,70}; //Change (Added 3 More Elements)
+
+    Display(Arr); // Display(100);  100 is a base address of the array //this is called as call by Addresss"""
+
+    return 0;
+}
