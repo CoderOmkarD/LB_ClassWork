02e34239eecd05bdf2866b0d0d036470c318861d
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-23 23:56:13 +0530

    Problems On N numbers

---
+++ program112.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+void Display(int *ptr) //Issue because *ptr always show the same output
+{
+    int iCnt = 0;
+
+    for (iCnt = 0; iCnt < 4; iCnt++)
+    {
+        printf("%d\n", *ptr);
+        // ptr++;
+    }
+}
+int main()
+{
+    int Arr[] = {10, 20, 30, 40};
+
+    Display(Arr); // Display(100);  100 is a base address of the array //this is called as call by Addresss"""
+
+    return 0;
+}
