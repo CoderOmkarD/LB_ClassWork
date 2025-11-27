38bba22c945e7ce49c35368be955b9f39bb73780
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-27 23:24:58 +0530

    Pattern Printing in Java

---
+++ program154.java
@@ -0,0 +1,38 @@
+
+//Input : 5
+//Output : 1 * 3 * 5 *
+import java.util.*;
+
+class Pattern {
+
+    public void Display(int iNo) {
+        int iCnt = 0;
+        for (iCnt = 1; iCnt <= iNo; iCnt++) 
+            {
+            if ((iCnt % 2) == 0) 
+            {
+                System.out.print("*\t");
+            } 
+            else 
+            {
+                System.out.print(iCnt + "\t");
+            }
+
+        }
+        System.out.println();
+    }
+}
+
+class program154 {
+
+    public static void main(String[] args) {
+        int iValue = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Frequency:");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
