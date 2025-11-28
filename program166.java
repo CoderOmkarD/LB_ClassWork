7b022521438464565a851924ec34fb100a474e5c
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-28 22:21:28 +0530

    Problems on Pattern Printing

---
+++ program166.java
@@ -0,0 +1,57 @@
+
+/*
+
+iRow :4 
+iCol :4
+
+Output : 
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+*/
+import java.util.Scanner;
+
+class Pattern {
+
+    public void Display(int iRow, int iCol) {
+        int i = 0, j = 0;
+
+        for (i = 1; i <= iRow; i++) 
+        {
+            for (j = 1; j <= iCol; j++) 
+            {
+                System.out.print(j + "\t");
+            }
+            System.out.println();
+
+        }
+    }
+
+}
+
+public class program166 {
+
+    public static void main(String[] args)
+
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Columns:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        sobj = null;
+        pobj = null;
+
+    }
+}
