d7ee8d8e045ff11f9570cbc48ee94f940c3510d2
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-01 17:33:59 +0530

    Problems On Pattern Printing

---
+++ program189.java
@@ -0,0 +1,75 @@
+
+/*
+
+iRow :6 
+iCol :6
+
+Output : 
+
+*       #       #       #       #       #
+*       *       #       #       #       #
+*       *       *       #       #       #
+*       *       *       *       #       #
+*       *       *       *       *       #
+*       *       *       *       *       *
+
+
+*/
+import java.util.Scanner;
+
+class Pattern {
+//Filter for square matrix
+    public void Display(int iRow, int iCol) {
+        int i = 0, j = 0;
+
+        if(iRow!=iCol)
+        {
+            System.out.println("Invalid Input");
+            System.out.println(" Value of row and column must be same:");
+            return;
+        }
+
+        for (i = 1; i <= iRow; i++) {
+
+            for (j = 1; j <= iCol; j++) 
+                {
+
+                if (i >=j) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("#\t");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+
+}
+
+public class program189 {
+
+    public static void main(String[] args)
+
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Columns:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        sobj = null;
+        pobj = null;
+
+    }
+}
