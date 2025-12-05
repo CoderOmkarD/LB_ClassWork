ae0d927ffb3768305016cbcf3347879fe6330876
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-05 11:21:15 +0530

    Problems On String

---
+++ program220.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int count(char str[])
+{   
+    int iCount=0;
+    while (*str != '\0')
+    {   
+        if(*str=='A' ||  *str=='a')
+        {
+             iCount++;
+        }
+        
+        str++;
+    }
+    return iCount;
+}
+
+int main()
+{
+    char Arr[] = {'\0'};
+    int iRet=0;
+
+    printf("Enter String:\n");
+    scanf("%[^'\n']s", Arr);
+
+    iRet =count(Arr);
+
+    printf("Count of 'A' is : %d",iRet);
+    return 0;
+}
