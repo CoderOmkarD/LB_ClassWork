8f5fe7b9f24803cc6b58adb53da0e02a2a9ae26d
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-06 09:22:24 +0530

    Problems On String

---
+++ program226.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int CountOccurance(char str[],char ch)
+{
+    int iCount = 0;
+    while (*str != '\0')
+    {
+        if ((*str == ch)) 
+            
+        {
+            iCount++;
+        }
+
+        str++;
+    }
+    return iCount;
+}
+
+int main()
+{
+    char Arr[] = {'\0'};
+    int iRet = 0;
+    char cValue='\0';
+
+
+    printf("Enter String:\n");
+    scanf("%[^'\n']s", Arr);
+
+    fflush(stdin);  //Not applicable on all OS
+
+    printf("Enter the character to get no.of occurences:\n");//it will not accept the character
+    scanf("%c",&cValue);
+
+    iRet = CountOccurance(Arr,cValue);
+
+    printf("Numbers of occurances are : %d", iRet);
+    return 0;
+}
