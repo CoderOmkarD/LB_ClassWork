c2ecd6125d81904d59361c729f016f203fcebec3
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-15 15:12:30 +0530

    Programs in C

---
+++ program47.c
@@ -0,0 +1,27 @@
+// Factors of 6
+
+#include <stdio.h>
+
+void DisplayFactors(int iNo)
+{
+    int iCnt=0;
+    printf("Factors of %d: \n ",iNo);
+    for (iCnt=1;iCnt<iNo;iCnt++)
+    {
+        if ((iNo % iCnt) == 0)
+        {
+            printf("%d\n",iCnt);
+        }
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter  Number :\n");
+    scanf("%d", &iValue);
+
+    DisplayFactors(iValue);
+
+    return 0;
+}
