f0b4db438e940353f1536e4dbae46e753c0373a2
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-15 15:12:47 +0530

    Programs in C

---
+++ program50.c
@@ -0,0 +1,34 @@
+// Factors of 6
+
+#include <stdio.h>
+
+void DisplayFactors(int iNo)
+{
+    int iCnt = 0;
+
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    printf("Factors of %d: \n", iNo);
+    for (iCnt = 1; iCnt <= (iNo / 2); iCnt++)
+    {
+        if ((iNo % iCnt) == 0)
+        {
+            printf("%d\n", iCnt);
+        }
+    }
+}
+
+// Time Complexity  :O(N/2)
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter  Number :\n");
+    scanf("%d", &iValue);
+
+    DisplayFactors(iValue);
+
+    return 0;
+}
