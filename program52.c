2e9fde94843cc7a2b3bc1a5bd7fd0d6c58ae51e4
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-16 20:54:08 +0530

    Problems On Number

---
+++ program52.c
@@ -0,0 +1,41 @@
+// Count of  Non factors
+
+#include <stdio.h>
+
+int CountNonFactors(int iNo)
+{
+    int iCnt = 0;
+    int iFrequency = 0;
+
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for (iCnt = 1; iCnt < iNo ; iCnt++)
+    {
+        if ((iNo % iCnt) != 0)
+        {
+            iFrequency++;
+        }
+    }
+
+    return iFrequency;
+
+}
+
+int main()
+{
+
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the Number \n");
+    scanf("%d", &iValue);
+
+    iRet = CountNonFactors(iValue);
+
+    printf("No. of Non Factors is :%d", iRet);
+
+    return 0;
+}
