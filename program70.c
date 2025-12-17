ce4fe00f87159f8250a8f806451c54f25725004e
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-17 23:04:19 +0530

    Programs On Numbers

---
+++ program70.c
@@ -0,0 +1,38 @@
+// Iteration
+//Sum of Digits
+#include <stdio.h>
+
+int SumOfDigits(int iNo)
+{
+    
+    int iSum = 0;
+    int iDigit =0;
+
+
+    while (iNo != 0)
+    {
+
+        iDigit=iNo%10;
+        iNo = iNo / 10;
+        iSum=iSum+iDigit;
+        
+    }
+
+    return iSum;
+}
+// Time Complexity:
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number:\n");
+    scanf("%d", &iValue);
+
+    iRet = SumOfDigits(iValue); // Call by Value
+
+    printf("Sum of Digits are : %d\n",iRet);
+
+    return 0;
+}
