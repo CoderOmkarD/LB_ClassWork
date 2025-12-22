bf29c618e18505ec983f81304bf246f264e5bb82
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-22 10:19:13 +0530

    Problems on N Numbers

---
+++ program105.java
@@ -0,0 +1,53 @@
+
+//OOP Design
+import java.util.*;
+
+class ArrayX {
+    public int Arr[];
+    public int iSize;
+    public int i = 0;
+
+    public ArrayX(int iNo) {
+        System.out.println("Inside Constructor");
+        iSize = iNo;
+        Arr = new int[iSize];
+    }
+
+    public void Accept() {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Elements of array");
+
+        for (i = 0; i < Arr.length; i++) {
+            Arr[i] = sobj.nextInt();
+        }
+    }
+
+    public void Display() {
+        System.out.println("Elements of the array are: ");
+
+        for (i = 0; i < Arr.length; i++) {
+            System.out.println(Arr[i]);
+        }
+    }
+}
+
+class program105 {
+
+    public static void main(String[] args) 
+    {
+        ArrayX aobj1 = new ArrayX(5);
+
+        System.out.println(aobj1.iSize);
+        
+        //Bad Programming Practise
+        //Need of Access Specifier
+        aobj1.iSize=11;
+        aobj1.Arr=null;
+        
+        ArrayX aobj2 = new ArrayX(7);
+
+        System.out.println(aobj2.iSize);
+    }
+
+}
