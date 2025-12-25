356cf49a6332f8f19a913020f901785133c577a9
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-25 18:52:31 +0530

    Program On N Numbers

---
+++ program134.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef  int * IPTR ;
+
+
+int main()
+{
+
+    int iLength=0;
+    IPTR iPtr=0;
+    int iCnt=0;
+
+    printf("Enten the no.of element\n");
+    scanf("%d",&iLength);
+
+
+    //step 1 : Allocate the memory
+    iPtr=(IPTR) malloc(iLength * sizeof(int));
+
+    if(NULL==iPtr)
+    {
+        printf("Enable to allocate the memory\n");
+        return -1;
+    }
+
+    printf("Enter the element:\n");
+
+    for (iCnt=0;iCnt<iLength;iCnt++)
+    {
+        scanf("%d",&iPtr[iCnt]);
+    }
+    
+    //step 2 : Use the memory 
+    //call to the function which contain business logic
+    //fun(iPtr,iLength);
+
+
+    //step 3 : Free the Memory
+    free(iPtr);
+
+
+    return 0;
+}
