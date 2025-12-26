cdafbd6aa6a07172fdee75854644640dc18f3f34
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-26 19:09:00 +0530

    Programs On N Numbers

---
+++ program150.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int *IPTR;
+//  < O(N)
+
+void ReverseDisplay(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    for (iCnt = iSize-1; iCnt >=0; iCnt--)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+}
+int Mininum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMin = 0;
+
+    for (iMin = Arr[0], iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if (Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+    return iMin;
+}
+
+int main()
+{
+
+    int iLength = 0;
+    IPTR iPtr = 0;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enten the no.of element\n");
+    scanf("%d", &iLength);
+
+    // step 1 : Allocate the memory
+    iPtr = (IPTR)malloc(iLength * sizeof(int));
+
+    if (NULL == iPtr)
+    {
+        printf("Enable to allocate the memory\n");
+        return -1;
+    }
+
+    printf("Enter the element:\n");
+
+    for (iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d", &iPtr[iCnt]);
+    }
+
+    // step 2 : Use the memory
+
+    ReverseDisplay(iPtr, iLength);
+
+    // step 3 : Free the Memory
+    free(iPtr);
+
+    return 0;
+}
