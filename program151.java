1b49a101d913f28200dfc1478bcb6db68fe1202d
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-28 21:17:08 +0530

    Pattern Printing In Java

---
+++ program151.java
@@ -0,0 +1,18 @@
+class Pattern {
+    public int iCnt = 0;
+
+    public void Display() {
+        
+        System.out.println("Inside Display");
+        // logic of pattern printing
+
+    }
+}
+
+class program151 {
+
+    public static void main(String[] args) {
+        Pattern pobj = new Pattern();
+        pobj.Display();
+    }
+}
