bd05e8c265a9c080c2cbfe5cfbc3fa86fdcfd486
Omkar  Durge <durgeomkar86@gmail.com>
2025-12-29 19:02:42 +0530

    Programs On Pattern Printing In Java

---
+++ program167.java
@@ -0,0 +1,57 @@
+
+/*
+
+iRow :4 
+iCol :4
+
+Output : 
+        1 1 1 1
+        2 2 2 2 
+        3 3 3 3 
+        4 4 4 4
+*/
+import java.util.Scanner;
+
+class Pattern {
+
+    public void Display(int iRow, int iCol) {
+        int i = 0, j = 0;
+
+        for (i = 1; i <= iRow; i++) 
+        {
+            for (j = 1; j <= iCol; j++) 
+            {
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+
+        }
+    }
+
+}
+
+public class program167 {
+
+    public static void main(String[] args)
+
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Columns:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        sobj = null;
+        pobj = null;
+
+    }
+}
